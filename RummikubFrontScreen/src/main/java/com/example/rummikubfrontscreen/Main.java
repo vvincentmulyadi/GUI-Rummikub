3f9ab867338eff873cc1880711709a50da2bd531
ulasawczuk <sawczuk.ula@gmail.com>
2023-09-27 14:48:30 +0200

    ff

--- RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/Main.java
+++ RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/Main.java
@@ -1,14 +1,10 @@
 package com.example.rummikubfrontscreen;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
     @Override
