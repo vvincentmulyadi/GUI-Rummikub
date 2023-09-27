101c90af6d7fc28ca5615c71393e2d6bcfe19287
vvincentmulyadi <144915089+vvincentmulyadi@users.noreply.github.com>
2023-09-27 14:25:16 +0200

    Added mouseClick recorder of coordinates in game-board.fxml

--- RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/BoardController.java
+++ RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/BoardController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class BoardController {
     @FXML
