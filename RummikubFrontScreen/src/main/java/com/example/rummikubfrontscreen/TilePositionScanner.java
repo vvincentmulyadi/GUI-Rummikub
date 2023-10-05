8d7f7d90a755e049364ea6fd3b768104cdfe6784
elias <eliasls2002@yahoo.de>
2023-10-05 16:36:16 +0200

    Board scanner

---
+++ RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/TilePositionScanner.java
@@ -0,0 +1,17 @@
+package com.example.rummikubfrontscreen;
+
+import com.example.rummikubfrontscreen.setup.Tile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TilePositionScanner {
+
+    public ArrayList<ArrayList<Tile>> boardScanner (initFXTile[] tile) {
+        int[] x = {1,2,3,4};
+        int[] y = {2,3,4,5};
+        // width 33, height 41 range 15
+        HashMap<Integer[], Tile[]> map = new HashMap<>();
+        for (int )
+    }
+}
