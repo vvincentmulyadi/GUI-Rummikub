17e29d9ae6662a5a8c27589c81e2fb56ce756d6e
Estebanix29 <115260182+Estebanix29@users.noreply.github.com>
2023-11-15 12:25:29 +0100

    node been translated to python

--- RummikubFrontScreen/target/classes/com/example/Node.py
+++ RummikubFrontScreen/target/classes/com/example/Node.py
@@ -1,1 +1,75 @@
-node = 1
+import random as r
+
+class Node:
+    def __init__(self, game, args, state, parent=None, action_taken=None):
+        self.game = game
+        self.args = args
+        self.state = state
+        self.parent = parent
+        self.action_taken = action_taken
+        
+        self.children = []
+        self.expandable_moves = game.get_valid_moves(state)
+        
+        self.visit_count = 0
+        self.value_sum = 0
+        
+    def is_fully_expanded(self):
+        return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
+    
+    def select(self):
+        best_child = None
+        best_ucb = -np.inf
+        
+        for child in self.children:
+            ucb = self.get_ucb(child)
+            if ucb > best_ucb:
+                best_child = child
+                best_ucb = ucb
+                
+        return best_child
+    
+    def get_ucb(self, child):
+        q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+        return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
+    
+    def expand(self):
+        action = np.random.choice(np.where(self.expandable_moves == 1)[0])
+        self.expandable_moves[action] = 0
+        
+        child_state = self.state.copy()
+        child_state = self.game.get_next_state(child_state, action, 1)
+        child_state = self.game.change_perspective(child_state, player=-1)
+        
+        child = Node(self.game, self.args, child_state, self, action)
+        self.children.append(child)
+        return child
+    
+    def simulate(self):
+        value, is_terminal = self.game.get_value_and_terminated(self.state, self.action_taken)
+        value = self.game.get_opponent_value(value)
+        
+        if is_terminal:
+            return value
+        
+        rollout_state = self.state.copy()
+        rollout_player = 1
+        while True:
+            valid_moves = self.game.get_valid_moves(rollout_state)
+            action = np.random.choice(np.where(valid_moves == 1)[0])
+            rollout_state = self.game.get_next_state(rollout_state, action, rollout_player)
+            value, is_terminal = self.game.get_value_and_terminated(rollout_state, action)
+            if is_terminal:
+                if rollout_player == -1:
+                    value = self.game.get_opponent_value(value)
+                return value    
+            
+            rollout_player = self.game.get_opponent(rollout_player)
+            
+    def backpropagate(self, value):
+        self.value_sum += value
+        self.visit_count += 1
+        
+        value = self.game.get_opponent_value(value)
+        if self.parent is not None:
+            self.parent.backpropagate(value)  
