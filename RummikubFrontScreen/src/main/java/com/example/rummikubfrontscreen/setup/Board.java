b67efdd25e05110cb8b098a67a41a0adc6e7a8d8
elias <eliasls2002@yahoo.de>
2023-12-07 13:22:30 +0100

    Board verifier ready

--- RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/setup/Board.java
+++ RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/setup/Board.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 
 public class Board {
 
-
-    //2D arraylist grid representing the runs and rows
+    // 2D arraylist grid representing the runs and rows
     private ArrayList<ArrayList<Tile>> currentGameBoard;
 
     public ArrayList<ArrayList<Tile>> getCurrentGameBoard() {
@@ -19,42 +18,49 @@ public class Board {
         this.currentGameBoard = currentGameBoard;
     }
 
-    //Constructor for the board
-    public Board(ArrayList<ArrayList<Tile>> listOfTiles){
+    // Constructor for the board
+    public Board(ArrayList<ArrayList<Tile>> listOfTiles) {
         currentGameBoard = listOfTiles;
     }
 
-    public Board(){};
+    public Board() {
+    };
 
-    public static boolean boardVerifier(ArrayList<ArrayList<Tile>> seriesInGame){
-        //convert(currentGameBoard);
-        Tile tile1 = null, tile2 = null;
-        for(int i = 0; i < seriesInGame.size();i++){
-            if (seriesInGame.get(i).size() < 3) {return false;}
-            //index of a first tl which is not a joker
+    public static boolean boardVerifier(ArrayList<ArrayList<Tile>> seriesInGame) {
+        // convert(currentGameBoard);
+        for (int i = 0; i < seriesInGame.size(); i++) {
+            if (seriesInGame.get(i).size() < 3) {
+                return false;
+            }
+
+            Tile tile1 = null, tile2 = null;
+            // index of a first tl which is not a joker
             int indexTile1 = 0;
-            //loop assigning values of first two tiles not being a joker
-            for(int j = 0; j < seriesInGame.get(i).size();j++){
-                if(seriesInGame.get(i).get(j).getValue().equals(Value.JOKER)) continue;
+            // loop assigning values of first two tiles not being a joker
+            for (int j = 0; j < seriesInGame.get(i).size(); j++) {
+                if (seriesInGame.get(i).get(j).getValue().equals(Value.JOKER))
+                    continue;
                 if (tile1 == null) {
                     tile1 = seriesInGame.get(i).get(j);
-                    indexTile1 = i;
-                }
-                else{
+                    indexTile1 = j;
+                } else {
                     tile2 = seriesInGame.get(i).get(j);
                     break;
                 }
             }
-            //case where there were two joker and one tl
-            if (tile2 == null) continue;
+            // case where there were two joker and one tl
+            if (tile2 == null)
+                continue;
 
-            //checking whether we are checking a group or a run
+            // checking whether we are checking a group or a run
             if (tile1.getValue() == tile2.getValue()) {
                 // Check group
-                if(!checkGroup(seriesInGame.get(i), tile1)) return false;
+                if (!checkGroup(seriesInGame.get(i), tile1))
+                    return false;
             } else {
                 // Check Run
-                if(!checkRun(seriesInGame.get(i), tile1, indexTile1)) return false;
+                if (!checkRun(seriesInGame.get(i), tile1, indexTile1))
+                    return false;
             }
             tile1 = null;
             tile2 = null;
@@ -62,38 +68,44 @@ public class Board {
         return true;
     }
 
-    private static boolean checkGroup (ArrayList<Tile> group, Tile tile1) {
+    private static boolean checkGroup(ArrayList<Tile> group, Tile tile1) {
         Value v = tile1.getValue();
         ArrayList<Colour> coloursUsed = new ArrayList<>();
-        if (group.size() > 4) return false;
+        if (group.size() > 4)
+            return false;
         for (int i = 0; i < group.size(); i++) {
-            if (group.get(i).getValue() == Value.JOKER) continue;
-            if (group.get(i).getValue() != v) return false;
-            if(coloursUsed.contains(group.get(i).getColour())) return false;
-            else coloursUsed.add(group.get(i).getColour());
+            if (group.get(i).getValue() == Value.JOKER)
+                continue;
+            if (group.get(i).getValue() != v)
+                return false;
+            if (coloursUsed.contains(group.get(i).getColour()))
+                return false;
+            else
+                coloursUsed.add(group.get(i).getColour());
         }
         return true;
     }
 
-    private static boolean checkRun (ArrayList<Tile> run, Tile tile1, int indexOfTile1){
+    private static boolean checkRun(ArrayList<Tile> run, Tile tile1, int indexOfTile1) {
         Colour c = tile1.getColour();
-        for(int i = 0; i<run.size(); i++){
-            if (run.get(i).getValue() == Value.JOKER) continue;
-            if (run.get(i).getColour() != c) return false;
-            if (i - indexOfTile1 + tile1.getValue().getValue()!= run.get(i).getValue().getValue()) return false;
+        for (int i = 0; i < run.size(); i++) {
+            if (run.get(i).getValue() == Value.JOKER)
+                continue;
+            if (run.get(i).getColour() != c)
+                return false;
+            if (i - indexOfTile1 + tile1.getValue().getValue() != run.get(i).getValue().getValue())
+                return false;
         }
         return true;
     }
-
-
 
     @Override
     public String toString() {
         String str = "";
-        for (int i = 0; i < currentGameBoard.size(); i ++) {
-            str+=  i + "  ";
+        for (int i = 0; i < currentGameBoard.size(); i++) {
+            str += i + "  ";
             str += currentGameBoard.get(i).toString() + "  ";
-            
+
             str += "\n";
         }
         return str;
@@ -101,33 +113,25 @@ public class Board {
 
     public static void main(String[] args) {
         ArrayList<Tile> group = new ArrayList<>();
-        Tile t1 = new Tile(Colour.BLACK, Value.THREE);
-        group.add(t1);
-        group.add(new Tile(Colour.BLACK, Value.FOUR));
-        group.add(new Tile(Colour.BLACK, Value.FIVE));
+
+        group.add(new Tile(Colour.BLUE, Value.SEVEN));
+        group.add(new Tile(Colour.RED, Value.SEVEN));
+        group.add(new Tile(Colour.BLACK, Value.SEVEN));
+        group.add(new Tile(Colour.YELLOW, Value.JOKER));
 
         ArrayList<Tile> run = new ArrayList<>();
-        run.add(new Tile(Colour.RED, Value.EIGHT));
-        run.add(new Tile(Colour.BLUE, Value.EIGHT));
-        run.add(new Tile(Colour.YELLOW, Value.EIGHT));
-        Tile t2 = new Tile(Colour.YELLOW, Value.NINE);
-        //run.add(t2);
-        //run.add(new Tile(Colour.YELLOW, Value.TEN));
-        //run.add(new Tile(Colour.BLACK, Value.ELEVEN));
-        //run.add(new Tile(Colour.BLACK, Value.JOKER));
-        //run.add(new Tile(Colour.BLACK, Value.TWELVE));
-        //run.add(new Tile(Colour.BLACK, Value.THIRTEEN));
-
+        run.add(new Tile(Colour.BLACK, Value.JOKER));
+        run.add(new Tile(Colour.BLACK, Value.JOKER));
+        run.add(new Tile(Colour.BLACK, Value.TWO));
+        run.add(new Tile(Colour.BLACK, Value.THREE));
+        run.add(new Tile(Colour.BLACK, Value.FOUR));
 
         Board board = new Board();
-        ArrayList<ArrayList<Tile>> seriesInGame =new ArrayList<>();
+        ArrayList<ArrayList<Tile>> seriesInGame = new ArrayList<>();
         seriesInGame.add(group);
         seriesInGame.add(run);
-        System.out.println(seriesInGame.size());
 
-        System.out.println(boardVerifier(seriesInGame));
-        //System.out.println(board.checkGroup(group, t1));
-        //System.out.println(board.checkRun(run, t2, 2));
+        System.out.println(Board.boardVerifier(seriesInGame));
     }
 
 }
