0e59394f85cf7add2157000f03b2433a35a2ca4f
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-01-22 22:25:20 +0100

    Update Main.java

--- RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/Main.java
+++ RummikubFrontScreen/src/main/java/com/example/rummikubfrontscreen/Main.java
@@ -10,15 +10,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage){
         try {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main .fxml"));
-        Scene scene = new Scene(fxmlLoader.load());*/
-        /*Group root = new Group();
-        Scene scene = new Scene(root, 600, 500, Color.GRAY);
-        Image icon = new Image("C:/Users/Lenovo/OneDrive/Desktop/png-clipart-rummy-rummikub-joker-tile-based-game-board-game-joker-game-face.png");
-        stage.getIcons().add(icon);
-        stage.setHeight(600);
-        stage.setWidth(500);
-        stage.setTitle("Rummikub");*/
 
             Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
             Scene scene1 = new Scene(root);
